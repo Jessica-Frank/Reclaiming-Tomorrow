68bc75f872d3020c8f052c24a6c657c79fb024f4
Jessica Frank <J_Frank@uncg.edu>
2023-09-25 13:10:02 -0400

    Created css file for unified project style

--- include/header.php
+++ include/header.php
@@ -3,10 +3,7 @@
         <a class="navbar-brand d-flex w-25 me-auto" href="/">Reclaiming Tomorrow</a>
         <ul class="nav justify-content-end">
             <li class="nav-item">
-                <a
-                <?php if (isset($on_rewards_page)) { echo "class=\"nav-link active\" " ;}
-                else { echo "class=\"nav-link\" " ;} ?>
-                href="/rewards/redemption">My Rewards</a>
+                <a class="nav-link" href="/rewards/redemption">My Rewards</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="/verify/login">Login</a>
