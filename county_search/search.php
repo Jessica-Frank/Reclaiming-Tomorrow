8a153cb7bf0878a33b06cbe2f99db959c7a04e20
kkirkley2019 <60412966+kkirkley2019@users.noreply.github.com>
2023-10-17 18:07:22 -0400

    Add files via upload

--- county_search/search.php
+++ county_search/search.php
@@ -1,94 +1,84 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
     <title>Reclaiming Tomorrow</title>
-
-  <meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="../verify/login.css">
-<style>
-body {
-    background-color: #FAF1E4;
-}
-
-.navbar {
-    background-color: #40513B;
-}
-.nav-link {
-    color: #FAF1E4;
-}
-.nav-link:hover {
-    color: #CEC5B7;
-}
-
-.navbar-brand {
-    color:#FAF1E4;
-}
-.navbar-brand:hover {
-    color: #CEC5B7;
-}
-/* Style The Dropdown Button */
-.dropbtn {
-  background-color: #40513B;
-  color: #FAF1E4;
-  padding: 16px;
-  font-size: 16px;
-  border: none;
-  cursor: pointer;
-}
-
-/* The container <div> - needed to position the dropdown content */
-.dropdown {
-  position: relative;
-  display: inline-block;
-}
-
-/* Dropdown Content  */
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #FAF1E4;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-}
-
-/* Links inside the dropdown */
-.dropdown-content a {
-  color: black;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-}
-
-/* Change color of dropdown links on hover */
-.dropdown-content a:hover {background-color: #609966}
-
-/* Show the dropdown menu on hover */
-.dropdown:hover .dropdown-content {
-  display: block;
-}
-
-/* Change the background color of the dropdown button when the dropdown content is shown */
-.dropdown:hover .dropbtn {
-  background-color: #40513B;
-}
-</style>
+    <style>
+        body {
+            background-color: #FAF1E4;
+        }
+        .navbar {
+            background-color: #40513B;
+        }
+        .nav-link {
+            color: #FAF1E4;
+        }
+        .nav-link:hover {
+            color: #CEC5B7;
+        }
+        .navbar-brand {
+            color: #FAF1E4;
+        }
+        .navbar-brand:hover {
+            color: #CEC5B7;
+        }
+    </style>
 </head>
 <body>
     <?php include '../include/header.php'; ?>
 
+    <h2>County Search</h2>
+    <p>Welcome to the County search page!</p>
+    <form method="post" action="search_county.php">
+        <label for="County">County Name</label>
+        <input type="text" id="County" name="County">
+        <input type="submit" value="Search">
+    </form>
 
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['County'])) {
+        $host = 'localhost';
+        $username = 'root';
+        $password = '';
+        $database = 'reclaiming_tomorrow_db';
 
-<h2> County Search</h2>
-<p> Welcome to the County search page!</p>
-<form>
-  <label for="county">County Name</label>
-  <input type="text" id="county" name="county">
-</form>
+        $conn = new mysqli($host, $username, $password, $database);
 
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $county = $_POST['County'];
+
+        $sql = "SELECT * FROM county_search WHERE County LIKE '%$county%'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo "<h3>Local Events</h3>";
+                echo "<p>" . $row['Local_event'] . "</p>";
+
+                echo "<h3>Associated Links</h3>";
+                echo "<p>" . $row['associated_links'] . "</p>";
+
+                echo "<h3>Pick-up Schedule</h3>";
+                echo "<p>" . $row['pick_up_schedule'] . "</p>";
+
+                echo "<h3>Buy Bin</h3>";
+                echo "<p>" . $row['buy_bin'] . "</p>";
+
+                echo "<h3>Alternatives</h3>";
+                echo "<p>" . $row['alternatives'] . "</p>";
+            }
+        } else {
+            echo "County not found or data not available.";
+        }
+
+        $conn->close();
+    }
+    ?>
 </body>
-
-</html> 
+</html>
---
+++ county_search/search_county.php
@@ -0,0 +1,42 @@
+ <?php
+    $host = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'reclaiming_tomorrow_db';
+
+    $conn = new mysqli($host, $username, $password, $database);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['County'])) {
+        $county = $_POST['County'];
+
+        $sql = "SELECT * FROM county_search WHERE County LIKE '%$county%'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo "<h3>Local Events</h3>";
+                echo "<p>" . $row['Local event'] . "</p";
+
+                echo "<h3>Associated Links</h3>";
+                echo "<p>" . $row['associated_links'] . "</p";
+
+                echo "<h3>Pick-up Schedule</h3>";
+                echo "<p>" . $row['pick_up_schedule'] . "</p";
+
+                echo "<h3>Buy Bin</h3>";
+                echo "<p>" . $row['buy_bin'] . "</p";
+
+                echo "<h3>Alternatives</h3>";
+                echo "<p>" . $row['alternatives'] . "</p";
+            }
+        } else {
+            echo "County not found or data not available.";
+        }
+    }
+
+    $conn->close();
+    ?>
