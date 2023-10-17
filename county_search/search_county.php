047b258d5600cccd5df74b1f1b602e3f7b13fc1b
kkirkley2019 <60412966+kkirkley2019@users.noreply.github.com>
2023-10-17 18:17:52 -0400

    Update search_county.php

--- county_search/search_county.php
+++ county_search/search_county.php
@@ -1,43 +1,47 @@
- <?php
-    $host = 'localhost';
-    $username = 'root';
-    $password = '';
-    $database = 'reclaiming_tomorrow_db';
+<?php
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'reclaiming_tomorrow_db';
 
-    $conn = new mysqli($host, $username, $password, $database);
+$conn = new mysqli($host, $username, $password, $database);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['County'])) {
-        $county = $_POST['County'];
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['County'])) {
+    $county = $_POST['County'];
 
-        $sql = "SELECT * FROM county_search WHERE County LIKE '%$county%'";
-        $result = $conn->query($sql);
+    $sql = "SELECT * FROM county_search WHERE County LIKE '%$county%'";
+    $result = $conn->query($sql);
 
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                echo "<h3>Local Events</h3>";
-                echo "<p>" . $row['Local event'] . "</p";
-
-                echo "<h3>Associated Links</h3>";
-                echo "<p>" . $row['Associated Links'] . "</p";
-
-                echo "<h3>Pick-up Schedule</h3>";
-                echo "<p>" . $row['Pick-up Scedule'] . "</p";
-
-                echo "<h3>Buy Bin</h3>";
-                echo "<p>" . $row['Buy Bins'] . "</p";
-
-                echo "<h3>Alternatives</h3>";
-                echo "<p>" . $row['Alternatives'] . "</p";
-            }
-        } else {
-            echo "County not found or data not available.";
+    if ($result->num_rows > 0) {
+        echo "<table>";
+        echo "<tr>";
+        echo "<th>Local Events</th>";
+        echo "<th>Associated Links</th>";
+        echo "<th>Pick-up Schedule</th>";
+        echo "<th>Buy Bin</th>";
+        echo "<th>Alternatives</th>";
+        echo "</tr>";
+        
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row['Local event'] . "</td>";
+            echo "<td>" . $row['Associated Links'] . "</td>";
+            echo "<td>" . $row['Pick-up Schedule'] . "</td>";
+            echo "<td>" . $row['Buy Bins'] . "</td>";
+            echo "<td>" . $row['Alternatives'] . "</td>";
+            echo "</tr>";
         }
+        
+        echo "</table>";
+    } else {
+        echo "County not found or data not available.";
     }
+}
 
-    $conn->close();
-    ?>
+$conn->close();
+?>
 
