53e2bf024f38f4a841300f7ec3b872251f073e29
kkirkley2019 <60412966+kkirkley2019@users.noreply.github.com>
2023-10-19 12:04:27 -0400

    added accepted materials

--- county_search/county.php
+++ county_search/county.php
@@ -8,27 +8,33 @@
  <style>
         
         table {
-            width: 50%; 
-            background-color: #FAF1E4; 
+            width: 80%; /* Reduce the width of the table */
+            background-color: #FAF1E4;
             color: #40513B;
-            border-collapse: collapse; 
+            border-collapse: collapse;
         }
 
         table, th, td {
-            border: 1px solid #40513B; 
-            padding: 10px; 
+            border: 1px solid #40513B;
+            padding: 5px; /* Reduce the padding for a smaller table */
         }
 
         th {
-            text-align: center; 
-            background-color: #40513B; 
-            color: #FAF1E4; 
+            text-align: center;
+            background-color: #40513B;
+            color: #FAF1E4;
             border: 1px solid #FAF1E4;
         }
+
     </style>
 </head>
 <body>
 <?php include '../include/header.php'; ?>
+
+
+
+
+
   <h2>County Search</h2>
     <p>Welcome to the County search page!</p>
 <form method="post" action="">
@@ -36,6 +42,11 @@
     <input type="text" name="County" id="County">
     <input type="submit" value="Search">
 </form>
+
+
+
+
+
 
 <?php
 $host = 'localhost';
@@ -61,15 +72,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['County'])) {
     if ($result->num_rows > 0) {
         echo "<table>";
         echo "<tr>";
+        echo "<th>Accepted Materials</th>";
         echo "<th>Local Events</th>";
         echo "<th>Associated Links</th>";
         echo "<th>Pick-up Schedule</th>";
         echo "<th>Buy Bin</th>";
-        echo "<th>Alternatives</th>";
+        echo "<th>Alternatives for Unaccpeted Materials</th>";
         echo "</tr>";
         
         while ($row = $result->fetch_assoc()) {
             echo "<tr>";
+          echo "<td>" . $row['Accepted Materials'] . "</td>";
             echo "<td>" . $row['Local event'] . "</td>";
              echo "<td><a href='" . $row['Associated Links'] . "'>" . $row['Associated Links'] . "</a></td>";
              echo "<td>" . $row['Pick-up Schedule'] . "</td>";
@@ -83,8 +96,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['County'])) {
         echo "County not found or data not available.";
     }
 }
+
 ?>
 
 
 </body>
 </html>
+
