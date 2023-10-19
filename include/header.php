52399437807693edb3c4c6cc9d249d42238c3956
kkirkley2019 <60412966+kkirkley2019@users.noreply.github.com>
2023-10-19 11:33:26 -0400

    Update header.php

--- include/header.php
+++ include/header.php
@@ -2,7 +2,7 @@
     <h2 class="logo">Reclaiming Tomorrow</h2>
     <nav class="navigation">
         <a href="../index.php">Home</a>
-        <a href="../county_search/search">Search</a>
+        <a href="../county_search/county">Search</a>
         <a href="#">Local Information</a>
         <a href="/rewards/redemption">Rewards</a>
         <a href="../verify/inbox">Contact Us</a>
