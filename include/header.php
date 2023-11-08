f3cce76190d71ac2d0fcb3a1c52afc72e539436f
Kiki <H_Huerta@uncg.edu>
2023-11-07 23:03:34 -0500

    fixed my error in header link

--- include/header.php
+++ include/header.php
@@ -2,7 +2,7 @@
     <h2 class="logo">Reclaiming Tomorrow</h2>
     <nav class="navigation">
         <a href="../index.php">Home</a>
-        <a href="../maps/locationator">Search</a>
+        <a href="../maps/search">Search</a>
         <a href="../county_search/county">County Information</a>
         <a href="/rewards/redemption">Rewards</a>
         <a href="../verify/inbox">Contact Us</a>
