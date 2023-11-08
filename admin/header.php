b49f4c52219652bb7313dc0e3e8804e99916e38d
Kiki <H_Huerta@uncg.edu>
2023-11-07 23:01:40 -0500

    Changed header links

--- admin/header.php
+++ admin/header.php
@@ -2,10 +2,10 @@
     <h2 class="logo">Reclaiming Tomorrow</h2>
     <nav class="navigation">
         <a href="../index.php">Home</a>
-        <a href="../county_search/search">Search</a>
-        <a href="#">Local Information</a>
+        <a href="../maps/locationator">Search</a>
+        <a href="../county_search/county">County Information</a>
         <a href="/rewards/redemption">Rewards</a>
-        <a href="#">Contact Us</a>
+        <a href="../verify/inbox">Contact Us</a>
         <a href="../admin_verify/admin_logout" class="btnLogin-popup">Logout</a>
     </nav>
 </header>
--- include/header.php
+++ include/header.php
@@ -2,8 +2,8 @@
     <h2 class="logo">Reclaiming Tomorrow</h2>
     <nav class="navigation">
         <a href="../index.php">Home</a>
-        <a href="../county_search/county">Search</a>
-        <a href="#">Local Information</a>
+        <a href="../maps/locationator">Search</a>
+        <a href="../county_search/county">County Information</a>
         <a href="/rewards/redemption">Rewards</a>
         <a href="../verify/inbox">Contact Us</a>
         <a href="../verify/login" class="btnLogin-popup">Login</a>
